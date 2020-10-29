97a6e3c5cd95f3debae931356cb42d7a3f8d2680
Dariusz Gafka <dgafka.mail@gmail.com>
2020-10-29 18:31:06 +0100

    Remove inherting from @inheritdoc as it is too complicated to understand

--- src/AnnotationFinder/TypeResolver.php
+++ src/AnnotationFinder/TypeResolver.php
@@ -25,17 +25,6 @@ class TypeResolver
         if ($analyzedClass->getName() !== $declaringClass->getName()) {
             return self::getMethodOwnerClass($declaringClass, $methodName);
         }
-        if (self::isInheritDocComment($methodReflection->getDocComment())) {
-            if ($analyzedClass->getParentClass() && $analyzedClass->getParentClass()->hasMethod($methodReflection->getName())) {
-                return self::getMethodOwnerClass($analyzedClass->getParentClass(), $methodName);
-            }
-            foreach ($analyzedClass->getInterfaceNames() as $interfaceName) {
-                if (method_exists($interfaceName, $methodReflection->getName())) {
-                    $reflectionClass = new \ReflectionClass($interfaceName);
-                    return self::getMethodOwnerClass($reflectionClass, $methodName);
-                }
-            }
-        }
         foreach ($analyzedClass->getTraits() as $trait) {
             if ($trait->hasMethod($methodReflection->getName()) && !self::wasTraitOverwritten($methodReflection, $trait)) {
                 return self::getMethodOwnerClass($trait, $methodName);
@@ -44,12 +33,7 @@ class TypeResolver
 
         return $analyzedClass;
     }
-
-    private static function isInheritDocComment(string $docBlock) : bool
-    {
-        return preg_match("/@inheritDoc/", $docBlock);
-    }
-
+    
     private static function getMethodDeclaringClass(\ReflectionClass $analyzedClass, string $methodName): \ReflectionClass
     {
         return $analyzedClass->getMethod($methodName)->getDeclaringClass();
